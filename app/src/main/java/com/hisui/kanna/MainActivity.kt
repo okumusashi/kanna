821ab0812c79ef0ea3f9d3c3b05d044c1da259e0
Lynn Sakashita <l.sakashita@speakbuddy.jp>
2022-11-23 21:38:53 +0900

    Initial commit

---
+++ app/src/main/java/com/hisui/kanna/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.hisui.kanna
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hisui.kanna.ui.theme.ShikiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShikiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting(text = "This is Kanna")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(text: String) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(
+            modifier = Modifier.align(Alignment.Center),
+            text = text
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ShikiTheme {
+        Greeting("Android")
+    }
+}

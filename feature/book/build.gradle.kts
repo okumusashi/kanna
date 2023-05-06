fc7f1c162bafec39c110372a2b4dc5f334363f89
Lynn Sakashita <dev.lynn.sakashita@gmail.com>
2023-05-06 13:34:25 +0900

    feat: add `NewBookScreen`

---
+++ feature/book/build.gradle.kts
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2022 Lynn Sakashita
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    id("kanna.android.feature")
+    id("kanna.android.library.compose")
+}
+
+android {
+    namespace = "com.hisui.kanna.feature.book"
+}
+
+dependencies {
+    implementation(libs.kotlinx.datetime)
+}
